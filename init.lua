6c65fb431d146c384da8212dbf220af14f29b5ce
Bastien Genin <bastien.genin@jdc.fr>
2023-09-18 00:02:37 +0200

    Add NvimTree

--- init.lua
+++ init.lua
@@ -141,7 +141,17 @@ require('lazy').setup({
       end,
     },
   },
-
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
