d74aaa2637025e96cbc20728bec03a71fe2e97e5
Bastien Genin <bastien.genin@jdc.fr>
2023-09-21 00:25:11 +0200

    Configure which-key

--- init.lua
+++ init.lua
@@ -110,7 +110,15 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      icons = {
+        separator = "---"
+      },
+    },
+  },
+
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
